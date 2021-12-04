5c263a6e04cb2759a7870f72a753f6267bae48fe
btea <2356281422@qq.com>
2021-12-04 22:27:24 +0800

    feat: add screenshot component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import TheWelcome from './components/TheWelcome.vue';
+import ScreenShotVue from './components/ScreenShot.vue';
 </script>
 
 <template>
-  <header>
+    <ScreenShotVue />
+    <!-- <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -14,68 +16,68 @@ import TheWelcome from './components/TheWelcome.vue'
 
   <main>
     <TheWelcome />
-  </main>
+  </main> -->
 </template>
 
 <style>
 @import './assets/base.css';
 
 #app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 2rem;
 
-  font-weight: normal;
+    font-weight: normal;
 }
 
 header {
-  line-height: 1.5;
+    line-height: 1.5;
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+    display: block;
+    margin: 0 auto 2rem;
 }
 
 a,
 .green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
+    text-decoration: none;
+    color: hsla(160, 100%, 37%, 1);
+    transition: 0.4s;
 }
 
 @media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
+    a:hover {
+        background-color: hsla(160, 100%, 37%, 0.2);
+    }
 }
 
 @media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
+    body {
+        display: flex;
+        place-items: center;
+    }
 
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
+    #app {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        padding: 0 2rem;
+    }
 
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+    header {
+        display: flex;
+        place-items: center;
+        padding-right: calc(var(--section-gap) / 2);
+    }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+    header .wrapper {
+        display: flex;
+        place-items: flex-start;
+        flex-wrap: wrap;
+    }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+    .logo {
+        margin: 0 2rem 0 0;
+    }
 }
 </style>
---
+++ src/components/ScreenShot.vue
@@ -0,0 +1,37 @@
+<template>
+    <button class="btn" @click="startShot">截图</button>
+    <div class="box">
+        <video autoplay class="box" ref="vBox"></video>
+    </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+const vBox = ref<HTMLVideoElement | null>(null);
+const startShot = async () => {
+    try {
+        vBox.value!.srcObject = await navigator.mediaDevices.getDisplayMedia({
+            video: true,
+            audio: false
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+</script>
+<style scoped>
+.btn {
+    --color: #6cf;
+    --bg-color: #66ccff10;
+    --hv-color: #66ccff40;
+    display: inline-block;
+    padding: 5px 20px;
+    cursor: pointer;
+    border: 1px solid var(--color);
+    background: var(--bg-color);
+    color: var(--color);
+    border-radius: 5px;
+}
+.btn:hover {
+    background: var(--hv-color);
+}
+</style>
