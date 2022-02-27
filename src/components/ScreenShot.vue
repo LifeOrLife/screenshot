b320c99ab3cf2c4dd3f3753e3e1e06eaff906185
btea <2356281422@qq.com>
2022-02-27 10:08:16 +0800

    chore: add @lifeorlife/message

--- src/components/ScreenShot.vue
+++ src/components/ScreenShot.vue
@@ -1,84 +1,86 @@
 <template>
-    <div class="ope-list">
-        <button class="btn" @click="start">截图</button>
-    </div>
-    <!-- <video ref="vBox" autoplay></video> -->
-    <preview-img v-if="showCropBox" :con="can" @close="closeCropBox"></preview-img>
+	<div class="ope-list">
+		<button class="btn" @click="start">截图</button>
+	</div>
+	<!-- <video ref="vBox" autoplay></video> -->
+	<preview-img v-if="showCropBox" :con="can" @close="closeCropBox"></preview-img>
 </template>
 <script setup lang="ts">
-import { ref, provide } from 'vue';
+import { ref, provide } from 'vue'
 import PreviewImg from './PreviewImg.vue'
-import { Canvas } from '@/utils/createCanvas';
-import { sleep } from '@/utils/sleep';
+import { Canvas } from '@/utils/createCanvas'
+import { sleep } from '@/utils/sleep'
 import { CROPOBJECT, Crop_Object } from '@/utils/inject'
-import Message from '@/utils/Message'
-const vBox = ref<HTMLVideoElement | null>(null);
+import LLmessage from '@lifeorlife/message'
+const vBox = ref<HTMLVideoElement | null>(null)
 const showCropBox = ref(false)
 const can = ref<HTMLCanvasElement>()
 const start = async () => {
-    const _c = new Canvas();
-    CROP_BOX.deal = _c;
-    can.value = _c.el
-    await startShot(_c.source);
-    await sleep(300);
-    _c.draw(_c.source)
-    showCropBox.value = true
-    // _c.export();
-    await stopShot(_c.source);
-};
+	const _c = new Canvas()
+	CROP_BOX.deal = _c
+	can.value = _c.el
+	await startShot(_c.source)
+	await sleep(300)
+	_c.draw(_c.source)
+	showCropBox.value = true
+	// _c.export();
+	await stopShot(_c.source)
+}
 const closeCropBox = () => {
-    showCropBox.value = false
+	showCropBox.value = false
 }
 const CROP_BOX: Crop_Object = {
-    closeCropBox
+	closeCropBox,
 }
 provide(CROPOBJECT, CROP_BOX)
 const startShot = async (source: HTMLVideoElement) => {
-    try {
-        source.srcObject = await navigator.mediaDevices.getDisplayMedia({
-            video: true,
-            audio: false
-        });
-        source.play();
-    } catch (error) {
-        console.log(error);
-        console.log('%c别截了，浏览器API调用有问题', 'color: red; font-size: 25px; margin: 5px 10px;')
-        new Message({
-            text: '当前环境不支持相关api',
-            background: 'error',
-            color: 'error'
-        })
-        throw Error('别截了，浏览器API调用有问题')
-    }
-};
+	try {
+		source.srcObject = await navigator.mediaDevices.getDisplayMedia({
+			video: true,
+			audio: false,
+		})
+		source.play()
+	} catch (error) {
+		console.log(error)
+		console.log('%c别截了，浏览器API调用有问题', 'color: red; font-size: 25px; margin: 5px 10px;')
+		LLmessage({
+			text: '当前环境不支持相关api',
+			style: {
+				background: '#e1b8b8',
+				color: 'red',
+			},
+		})
+		throw Error('别截了，浏览器API调用有问题')
+	}
+}
 
 const stopShot = (source: HTMLVideoElement) => {
-    const srcObject = source.srcObject;
-    if (srcObject && 'getTracks' in srcObject) {
-        const tracks = srcObject.getTracks();
-        tracks.forEach((track) => track.stop())
-        source.srcObject = null;
-    }
+	const srcObject = source.srcObject
+	if (srcObject && 'getTracks' in srcObject) {
+		const tracks = srcObject.getTracks()
+		tracks.forEach(track => track.stop())
+		source.srcObject = null
+	}
 }
 </script>
 <style scoped>
-.ope-list{
-    text-align: center;
-    margin: 20px;
+.ope-list {
+	text-align: center;
+	margin: 20px;
 }
 .btn {
-    --color: #6cf;
-    --bg-color: #66ccff10;
-    --hv-color: #66ccff40;
-    display: inline-block;
-    padding: 5px 20px;
-    cursor: pointer;
-    border: 1px solid var(--color);
-    background: var(--bg-color);
-    color: var(--color);
-    border-radius: 5px;
+	--color: #6cf;
+	--bg-color: #66ccff10;
+	--hv-color: #66ccff40;
+	display: inline-block;
+	padding: 5px 20px;
+	cursor: pointer;
+	border: 1px solid var(--color);
+	background: var(--bg-color);
+	color: var(--color);
+	border-radius: 5px;
 }
 .btn:hover {
-    background: var(--hv-color);
+	background: var(--hv-color);
 }
 </style>
